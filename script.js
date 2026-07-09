c973dfb5bf0edd5416676bc78db66eeb52eec246
Sumon Sarkar <sarkar.sumon2468@gmail.com>
2026-07-10 01:10:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+fetch("videos.json")
+.then(response => response.json())
+.then(videos => {
+
+    const container = document.getElementById("videos");
+
+    videos.forEach(video => {
+
+        container.innerHTML += `
+        <div class="card">
+
+            <h2>${video.title}</h2>
+
+            <p>📅 ${video.date}</p>
+
+            <p>📦 ${video.size}</p>
+
+            <a href="${video.link}" target="_blank">
+                <button>⬇ DOWNLOAD</button>
+            </a>
+
+        </div>
+        `;
+
+    });
+
+});
